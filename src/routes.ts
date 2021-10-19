221dfca4e21e03a350d3a22fdb3ab2085ee10a2e
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2021-10-19 00:25:15 -0300

    feat: add ensureAuthenticated na rota profile

--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,6 @@ router.post("/messages", ensureAuthenticated ,new CreateMessageController().hand
 
 router.get("/messages/last3", new GetLast3MessagesController().handle);
 
-router.get("/profile", new ProfileUserController().handle); /* precisa do ensureAuthenticated mas da erro */
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle); /* precisa do ensureAuthenticated mas da erro */
 
 export { router } 
